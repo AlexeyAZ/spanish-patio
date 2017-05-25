7b6661befabb7ac1000fe8a6356a677d0c176ca6
Alexey <teos.nl@gmail.com>
2017-05-25 12:21:14 +0300

    100%

--- build/php/sendpresent.php
+++ build/php/sendpresent.php
@@ -1,5 +1,5 @@
 <?php
-$sendto = "alex_z@franch5.ru, zvnfranch5@yandex.ru, franchaizing-5@yandex.ru";
+$sendto = "alex_z@franch5.ru, zvnfranch5@yandex.ru, franchaizing-5@yandex.ru, 214824@bk.ru";
 $city = nl2br($_POST['city']);
 $age = nl2br($_POST['age']);
 $business = nl2br($_POST['business']);
